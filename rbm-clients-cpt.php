9c90c84322f279534488899c78a34a084dc63d82
Eric Defore <eric.defore@gmail.com>
2017-02-28 12:31:25 -0500

    v0.1.1

--- rbm-clients-cpt.php
+++ rbm-clients-cpt.php
@@ -3,7 +3,7 @@
 Plugin Name: RBM Clients CPT
 Plugin URL: https://github.com/realbig/RBM-Clients-CPT
 Description: Clients CPT moved from CPT-onomies
-Version: 0.1.0
+Version: 0.1.1
 Text Domain: rbm-clients-cpt
 Author: Eric Defore
 Author URL: http://realbigmarketing.com
